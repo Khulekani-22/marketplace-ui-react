1a85840cda89a3e1fc32c83588abddf95b1b1754
Khulekani-22 <khulekani@22onsloane.co>
2025-09-28 14:10:02 +0200

    Less errors (Storage persists better)

--- dist/assets/useAppSync-Cv6kC8Or.js
+++
@@ -1,1 +0,0 @@
-import{r as t,A as e}from"./index-CmOqa4Kr.js";const n=()=>{const r=t.useContext(e);if(!r)throw new Error("useAppSync must be used inside <AppSyncProvider />");return r};export{n as u};
