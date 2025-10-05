16d19cc4b4e818a24afd8436adb664254222eee5
Khulekani-22 <khulekani@22onsloane.co>
2025-10-05 18:35:57 +0200

    (Updated for vercel)

---
+++ dist/assets/useWallet-DN1Oz1sr.js
@@ -0,0 +1,1 @@
+import{r as e,W as r}from"./index-CDEs_jt0.js";function s(){const t=e.useContext(r);if(!t)throw new Error("useWallet must be used within <WalletProvider />");return t}export{s as u};
