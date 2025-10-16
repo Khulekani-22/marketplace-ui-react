7e58eeb851892f11e5377c7c5e1331d40a7d3d1b
Khulekani-22 <khulekani@22onsloane.co>
2025-10-16 16:05:43 +0200

    fix: Rename uuidv4 import to uuid to match function calls
    - Changed 'uuidv4' import alias to 'uuid' to match uuid() calls in code
    - Fixes 'uuid is not defined' error when booking sessions
    - Resolves Network Error when trying to book a session

--- backend/routes/subscriptions.js
+++ backend/routes/subscriptions.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuid } from "uuid";
 import { getData, saveData } from "../utils/hybridDataStore.js";
 import { firebaseAuthRequired } from "../middleware/authFirebase.js";
 
